d1bd4f746d3bb6eb8b9975e8cea7e70f8deb4b20
pedrochagas6 <pedrochagas6@icloud.com>
2020-03-29 16:33:43 -0300

    Movendo arquivos de diretorio

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  fs.readFile('index.html', function(error, data){
+    if(error) {
+      res.writeHead(404)
+      res.write('Error: File Not Found')
+    } else {
+      res.write(data) 
+    }
+    res.end()
+  })
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+}); 
