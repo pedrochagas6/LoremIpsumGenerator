da393c2d862ae6acd0e27faaf54699e4f0c6bff4
pedrochagas6 <pedrochagas6@icloud.com>
2020-04-18 20:31:28 -0300

    P

---
+++ GenerateLoremIpsum.js
@@ -0,0 +1,17 @@
+import { LoremIpsum } from "lorem-ipsum";
+//const LoremIpsum = require("lorem-ipsum").LoremIpsum;
+
+const lorem = new LoremIpsum({
+    sentencesPerParagraph: {
+        max: 8,
+        min: 4
+    },
+    wordsPerSentence: {
+        max: 16,
+        min: 4
+    }
+});
+
+function generateParagraphs(aNumberOfParagraphs) {
+    document.body.append("<p>" + lorem.generateParagraphs(aNumberOfParagraphs) + "</p>");
+}
